72e8a1d2a43708e196f9b91963ca81a32bc92ba1
Ben <bgrowcott@hotmail.co.uk>
2022-05-02 14:14:13 +0100

    created models -  but running server isn't creating db

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const validator = require('validator')
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "invalid email"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
