a31982b54897f30ec7be31f45c2b1241569329e3
Ben <bgrowcott@hotmail.co.uk>
2022-05-03 10:24:20 +0100

    added basic route and controller

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-// const activity = cwd.includes('01-Activities')
-//   ? cwd.split('/01-Activities/')[1]
-//   : cwd;
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('/01-Activities/')[1]
+  : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once('open', () => {
+  // console.log(db.models)
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`${activity} - API server running on port ${PORT}!`);
   });
 });
